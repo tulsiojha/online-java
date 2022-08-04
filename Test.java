b357b16ef71f45e1fdff90a0f3d5e09b394da40a
tulsiojha <tulsiojhaamg@gmail.com>
2022-08-04 21:00:06 +0545

    test

---
+++ Test.java
@@ -0,0 +1,35 @@
+public class Test{
+    
+    public static void main(String[] args){
+        
+        Hello h = new Hello();
+
+    }
+}
+
+class Hello{
+
+    Hello(){
+    
+        this.upper("Ramesh");
+    } 
+
+
+    void upper(String name){
+
+        char[] sfsdf = "abc";
+
+
+        char[] tempName = name.toCharArray();
+        for(int i = 0; i <= name.length()-1; i++){
+
+            if((char)((byte)name.charAt(i)) > 96 && (char)((byte)name.charAt(i)) < 123){
+                tempName[i] = (char)((byte)name.charAt(i)-32);
+            }
+            
+        }
+
+        System.out.println(tempName);
+    }
+
+}
